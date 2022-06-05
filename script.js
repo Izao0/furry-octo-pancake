c34011875cc1c21ba184b185b5e3a4cb3fe613f4
Izao00 <work_mail04@mail.ru>
2022-06-05 16:01:12 +0500

    lesson 4

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 const goods = [
-  { title: 'Shirt', price: 150 },
-  { title: 'Socks', price: 50 },
-  { title: 'Jacket', price: 350 },
-  { title: 'Shoes', price: 250 },
+  { title: "Shirt", price: 150 },
+  { title: "Socks", price: 50 },
+  { title: "Jacket", price: 350 },
+  { title: "Shoes", price: 250 },
 ];
 
-const BASE_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+const BASE_URL =
+  "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses";
 const GOODS = `${BASE_URL}/catalogData.json`;
-const GET_BASKET_GOODS_ITEMS = `${BASE_URL}/getBasket.json`
+const GET_BASKET_GOODS_ITEMS = `${BASE_URL}/getBasket.json`;
 
-function service(url, callback) { //Будет использовать 
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url)
+function service(url) {
+  return new Promise((resolve) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.send();
+    xhr.onload = () => {
+      resolve(JSON.parse(xhr.response));
+    };
+  });
+}
+/*
+function service(url, callback) {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", url);
   const loadHangler = () => {
     callback(JSON.parse(xhr.response));
-  }
-  xhr.onload = loadHangler
+  };
+  xhr.onload = loadHangler;
 
   xhr.send();
-}
+}*/
 class GoodsItem {
   constructor({ product_name, price }) {
     this.title = product_name;
@@ -35,24 +47,34 @@ class GoodsItem {
 }
 class GoodsList {
   items = [];
+  filteredItems = [];
   fetchGoods(callback = () => {}) {
-    service(GOODS, data => {
+    const prom = service(GOODS);
+    prom.then((data) => {
       this.items = data;
+      this.filteredItems = data;
       callback();
+    });
+  }
+  filterItems(value) {
+    this.filteredItems = this.items.filter(({ product_name }) => {
+      return product_name.match(new RegExp(value, "gui"));
     });
   }
   calculatePrice() {
     return this.items.reduce((prev, { price }) => {
       return prev + price;
-    }, 0)
+    }, 0);
   }
   render() {
-    const goods = this.items.map(item => {
-      const goodItem = new GoodsItem(item);
-      return goodItem.render()
-    }).join('');
-  
-    document.querySelector('.goods-list').innerHTML = goods;
+    const goods = this.filteredItems
+      .map((item) => {
+        const goodItem = new GoodsItem(item);
+        return goodItem.render();
+      })
+      .join("");
+
+    document.querySelector(".goods-list").innerHTML = goods;
   }
 }
 
@@ -72,6 +94,14 @@ basketGoods.fetchGoods();
 const goodsList = new GoodsList();
 goodsList.fetchGoods(() => {
   goodsList.render();
-})
+});
 goodsList.fetchGoods();
 goodsList.render();
+
+document
+  .getElementsByClassName("search-button")[0]
+  .addEventListener("click", () => {
+    const value = document.getElementsByClassName("goods-search")[0].value;
+    goodsList.filterItems(value);
+    goodsList.render();
+  });
