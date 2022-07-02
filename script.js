75c50ee30545f2459c0bec8af7ca2c9305f1b989
Izao0 <103335863+Izao0@users.noreply.github.com>
2022-07-02 14:08:46 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-const BASE_URL =
-  "http://localhost:8000/";
-const GOODS = `${BASE_URL}/goods.json`;
+const BASE_URL = "http://localhost:8000/";
+const GOODS = `${BASE_URL}goods.json`;
 const GET_BASKET_GOODS_ITEMS = `${BASE_URL}basket`;
 
 function service(url) {
@@ -8,61 +7,78 @@ function service(url) {
 }
 
 function init() {
-
-
-  Vue.component('custom-search', {
+  Vue.component("custom-search", {
     template: `
     <input type="text" class="goods-search" @input="$emit('input', $event.target.value)"/>
-    `
-  })
-
-  Vue.component('basket', {
-    template: `
-      <div class="fixed-area">
-         <div class="basket-card">
-            <div class="basket-card__header">
-               <h1 class="basket-card__header__title">basket card</h1>
-               <div class="basket-card__header__delete-icon"
-                  v-on:click="$emit('close')"
-               ></div>
-            </div>
-            <div class="basket-card__content">
-              <p class="basket-card__content__title">product 1<p>
-              <span class="basket-card__price">200<span>
-              <span class="basket-card__count"> 1шт.</span>
-              <button class="basket-card__add">+</button>
-              <button class="basket-card__remove">-</button>
-            </div>
-         </div>
-      </div>
     `,
-    mounted(){
-      service(GET_BASKET_GOODS_ITEMS).then(() => {
-        this.basketGoodsItems = data;
-      })
-    }
-  })
+  });
 
-  Vue.component('custom-button', {
-    template: `
-    <button class="search-button" type="button" v-on:click="$emit('click')">
-      <slot>Добавить</slot>
-    </button>
-    `
-  })
   Vue.component("good", {
-    props:[
-      'item'
-    ],
+    props: ["item"],
     template: `
     <div class="goods-item">
       <h3>{{ item.product_name }}</h3>
       <p>{{ item.price }}</p>
-      <div>
-        <custom-button></custom-button>
-      </div>
     </div>
     `,
+  });
+  const CustomButton = Vue.component("search-button", {
+    template: `
+    <button class="search-button" type="button" v-on:click="$emit('click')">
+       <slot></slot>
+    </button>
+  `,
+  });
+  const basketItem = Vue.component("basket-item", {
+    props: ["item"],
+    template: `
+    <div class="basket-item">
+      <span class="item__title"> {{ item.product_name }}</span>
+      <span class="item__price">( {{ item.price }} )</span>
+      <div class="basket-item__count">
+        <span>{{ item.count }} шт.</span>
+        <button>+</button>
+        <button>-</button>
+      </div>
+    </div>
+    `
+    
+  })
+  const basketGoods = Vue.component("basket", {
+    data() {
+      return {
+        basketGoodsItems: [],
+      };
+    },
+    template: `
+    <div class="fixed-area">
+       <div class="basket-card">
+          <div class="basket-card__header">
+             <h1 class="basket-card__header__title">basket card</h1>
+             <div class="basket-card__header__delete-icon"
+                v-on:click="$emit('close')"
+             ></div>
+          </div>
+          <div class="basket-card__content">
+            <basket-item v-for="item in basketGoodsItems" :item="item"></basket-item>
+          </div>
+       </div>
+    </div>
+    `,
+    mounted() {
+      service(GET_BASKET_GOODS_ITEMS).then((data) => {
+        this.basketGoodsItems = data;
+      })
+    },
+  });
+
+  const goodsItem = Vue.component("goods-item", {
+    props: ["item"],
+    template: `
+    <div class="goods-item">
+      <p>{{ item.price }}</p>
+    </div>
+  `,
   });
   const app = new Vue({
     el: "#root",
